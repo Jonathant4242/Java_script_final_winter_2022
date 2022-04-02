f97129a5f4a4d882bdf2ffb3c67fa7728d8d594e
John T <91383892+Jonathant4242@users.noreply.github.com>
2022-04-02 10:46:56 -0700

    Add files via upload

---
+++ week02/scripts/main.js
@@ -0,0 +1,7 @@
+// toggle menu in small view
+const toggleMenu = () => {
+    document.querySelector('#menu').classList.toggle('open');
+}
+let element.name("Jonathan Trok")
+document.querySelector('#toggleMenu').addEventListener('click', toggleMenu);
+
